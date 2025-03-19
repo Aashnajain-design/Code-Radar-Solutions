04cdb47979fb107b83597b00645f6d44033346eb
Aashna jain <jainaashna714@gmail.com>
2025-03-19 16:23:04 +0530

    Solution to Input and Print a Double

--- Input and Print a Double@@6788d5012db25efec946f942/code.c
+++ Input and Print a Double@@6788d5012db25efec946f942/code.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
+#include<locale.h>
 int main()
 {
+    setlocale(LC_NUMERIC, "C"); // Sets the locale to "C" (dot as the decimal separator)
+
     float a = 3.1415;
     scanf("%f", &a);
-    printf("You entered: %.4f", a);
+    printf("You entered: %.4f\n", a);
     return 0;
 }
